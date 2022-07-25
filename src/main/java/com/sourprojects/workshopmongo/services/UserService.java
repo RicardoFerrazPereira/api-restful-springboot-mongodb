31dd97978d5bb26b2c2ccefcd9a046d5a787b360
RicardoFerraz <sourvivens@gmail.com>
2022-07-25 09:33:11 -0300

    Obtendo um usuário por id

--- src/main/java/com/sourprojects/workshopmongo/services/UserService.java
+++ src/main/java/com/sourprojects/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.sourprojects.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sourprojects.workshopmongo.domain.User;
 import com.sourprojects.workshopmongo.repository.UserRepository;
+import com.sourprojects.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) { 
+		  Optional<User> obj = repo.findById(id); 
+		  return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+		} 
 
 }
---
+++ src/main/java/com/sourprojects/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.sourprojects.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+
+}
