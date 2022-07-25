f922b433a520ae390d049a22326eb02a8d2d101b
RicardoFerraz <sourvivens@gmail.com>
2022-07-25 14:58:38 -0300

    Consulta simples com query methods

--- src/main/java/com/sourprojects/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/sourprojects/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.sourprojects.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,7 @@ import com.sourprojects.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/sourprojects/workshopmongo/resourse/PostResource.java
+++ src/main/java/com/sourprojects/workshopmongo/resourse/PostResource.java
@@ -1,14 +1,17 @@
 package com.sourprojects.workshopmongo.resourse;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sourprojects.workshopmongo.domain.Post;
-
+import com.sourprojects.workshopmongo.resourse.util.URL;
 import com.sourprojects.workshopmongo.services.PostService;
 
 
@@ -23,6 +26,13 @@ public class PostResource {
 	public ResponseEntity <Post> findById(@PathVariable String id) {		
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
