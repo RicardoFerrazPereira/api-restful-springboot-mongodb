d6f638c7ecffcae297c7fb321589a1105a35b742
RicardoFerraz <sourvivens@gmail.com>
2022-07-25 13:29:11 -0300

    Obtendo um post por id

---
+++ src/main/java/com/sourprojects/workshopmongo/resourse/PostResource.java
@@ -0,0 +1,28 @@
+package com.sourprojects.workshopmongo.resourse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sourprojects.workshopmongo.domain.Post;
+
+import com.sourprojects.workshopmongo.services.PostService;
+
+
+@RestController
+@RequestMapping("/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity <Post> findById(@PathVariable String id) {		
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/sourprojects/workshopmongo/services/PostService.java
@@ -0,0 +1,34 @@
+package com.sourprojects.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sourprojects.workshopmongo.domain.Post;
+
+import com.sourprojects.workshopmongo.repository.PostRepository;
+import com.sourprojects.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+//	public Post findById(String id) { 
+//		  Optional<Post> obj = repo.findById(id);
+//		  if(obj == null) {
+//			throw new ObjectNotFoundException("Objeto não encotrado");  
+//		  }
+//		  return obj;
+//	}
+	
+	public Post findById(String id) { 
+		  Optional<Post> obj = repo.findById(id); 
+		  return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+				  
+				  
+
+}
